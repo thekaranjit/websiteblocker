e504e9b161e5a61ade164395f8629c88e79ac0e2
thekaranjit <thekaranjit@gmail.com>
2018-08-14 03:51:38 +0400

    First Part

---
+++ webblocker.py
@@ -0,0 +1,12 @@
+import time
+from datetime import datetime as dt
+host_path="C:\Windows\System32\drivers\etc\hosts"
+
+redirect="127.0.0.1"
+
+website_list=["www.facebook.com","facebook.com","thekaranjit.mail.liv.com"]
+
+while True:
+    if dt(dt.now().year,dt.now().month,dt.now().day,8) < dt.now() < dt.now().year,dt.now().month, dt.now().day,16):
+    print(1)
+    time.sleep(5)
